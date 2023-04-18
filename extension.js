0240d6677c0caeaa61fd5e03bdaf427e157ab66d
Alexis Purslane <alexispurslane@localhost.localdomain>
2023-04-18 18:36:10 -0400

    Correctly keep track of the addition and deletion of windows and workspaces (includes moving windows and workspaces as well). A significant step!

--- extension.js
+++ extension.js
@@ -22,7 +22,7 @@ const GETTEXT_DOMAIN = 'my-indicator-extension';
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
-const { GObject, Gio, St } = imports.gi;
+const { GObject, Gio, St, Meta, Shell } = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
@@ -35,6 +35,116 @@ const _ = ExtensionUtils.gettext;
 
 const ICON_AUTO_ON = Gio.icon_new_for_string(`${Me.path}/icons/auto-on-symbolic.svg`);
 const ICON_AUTO_OFF = Gio.icon_new_for_string(`${Me.path}/icons/auto-off-symbolic.svg`);
+
+const TilingMode = {
+    Stack: 'stack',
+    Tab: 'tab',
+    Split: 'split'
+};
+
+const NodeTypes = {
+    Leaf: 'leaf',
+    Branch: 'branch'
+};
+
+const Orientation = {
+    H: 'horizontal',
+    V: 'vertical'
+};
+
+class AutoTilingLayoutHandler {
+    constructor() {
+        // We represent layouts as an imperfect binary tree where leaves are a
+        // stack of windows (so we can do i3 style stacking or tabs, like Pop
+        // Shell does) and branches represent a split with a ratio. (Orientation
+        // is decided by the size of the containing container, as in Pop Shell.)
+        //
+        // type Container = { type: Leaf, windows: [Window] } | {
+        //     type: Branch,
+        //     containers: [Container, Container],
+        //     ratio: Float,
+        //     orientation: Orientation
+        // }
+        // layouts :: [{ container: Container, workspace: Workspace }]
+        this._layouts = [];
+        for (let i = 0; i < global.workspace_manager.n_workspaces; i++) {
+            this._addWorkspace(i);
+        }
+        global.workspace_manager.connect('notify::n-workspaces',
+            this._nWorkspacesChanged.bind(this));
+    }
+
+    _nWorkspacesChanged() {
+        let newNumWorkspaces = global.workspace_manager.n_workspaces;
+        let oldNumWorkspaces = this._layouts.length;
+        if (newNumWorkspaces > oldNumWorkspaces) {
+            // assumes workspaces are always added on the end
+            for (let i = oldNumWorkspaces; i < newNumWorkspaces; i++) {
+                this._addWorkspace(i);
+            }
+        } else if (newNumWorkspaces < oldNumWorkspaces) {
+            let removedNum = oldNumWorkspaces - newNumWorkspaces;
+            let removedIndex;
+            for (let i = 0; i < oldNumWorkspaces; i++) {
+                let workspace = global.workspace_manager.get_workspace_by_index(i);
+                if (this._layouts[i].workspace != workspace) {
+                    removedIndex = i;
+                    break;
+                }
+            }
+
+            this._removeWorkspaces(removedIndex, removedNum);
+        }
+    }
+
+    _addWorkspace(i) {
+        log(`added workspace: ${i}`);
+        // Get the workspace for this index
+        let workspace = global.workspace_manager.get_workspace_by_index(i);
+
+        if (!workspace) {
+            logError(`workspace ${i} not defined in Workspace Manager, got ${workspace}`, 'window manager error');
+            return;
+        }
+
+        // Create a layout for this workspace with no windows on it
+        this._layouts[i] = {
+            container: { type: NodeTypes.Leaf, windows: [] },
+            workspace
+        };
+
+        // Add all the windows on this workspace to this workspace's layout
+        for (let actor in global.get_window_actors()) {
+            if (typeof actor == "object" && actor.meta_window_get_type() == Meta.WindowType.NORMAL) {
+                let meta_window = actor.get_meta_window();
+                if (meta_window.on_all_workspaces() || meta_window.get_workspace().index() != i)
+                    continue;
+                this._windowAdded(workspace, actor);
+            }
+        }
+
+        // Keep track of any new/deleted windows
+        workspace.connectObject(
+            'window-added', this._windowAdded.bind(this),
+            'window-removed', this._windowRemoved.bind(this),
+            this
+        );
+    }
+
+    _removeWorkspaces(startIdx, num) {
+        log(`removed workspaces ${startIdx}-${startIdx + num - 1}`);
+        let removedWorkspaces = this._layouts.splice(startIdx, num);
+        removedWorkspaces.forEach(l => l.workspace.disconnectObject(this));
+    }
+
+    _windowAdded(workspace, window) {
+        log(`window '${window.title}' added on workspace ${workspace.workspace_index}`);
+    }
+
+    _windowRemoved(workspace, window) {
+        log(`window '${window.title}' removed on workspace ${workspace.workspace_index}`);
+    }
+}
 
 const AutoTilingToggle = GObject.registerClass(
     class AutoTilingToggle extends QuickSettings.QuickToggle {
@@ -64,6 +174,8 @@ const AutoTilingToggle = GObject.registerClass(
 class Extension {
     constructor(uuid) {
         this._uuid = uuid;
+        this._quickSettingToggle = new AutoTilingToggle();
+        this._layoutHandler = new AutoTilingLayoutHandler();
 
         ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
     }
@@ -71,7 +183,6 @@ class Extension {
     enable() {
         log("enabling");
         log("adding toggle to quick settings menu");
-        this._quickSettingToggle = new AutoTilingToggle();
         QuickSettingsMenu._addItems([this._quickSettingToggle]);
         QuickSettingsMenu.menu._grid.set_child_below_sibling(
             this._quickSettingToggle,
