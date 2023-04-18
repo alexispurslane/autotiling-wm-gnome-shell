318acb090cb19cc7fc6dc1f0110233e5da8871fb
Alexis Purslane <alexispurslane@localhost.localdomain>
2023-04-17 20:18:47 -0400

    Fix prefs indentation, add README.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Auto Tiling
+## A GNOME extension providing automatic tiling window management in the style of the Pop Shell tiling extension
+
+With Pop OS planning a move to the COSMIC DE, leaving their GNOME extensions behind unmaintained, and their extension already incompatible with GNOME 44, what will happen to those of us who wish to remain on GNOME while maintaining that automatic tiling goodness? That's what this project aims to answer. We want to create a simple, stable, no-frills replacement for the Pop Shell extension that is future-proofed for all future GNOME versions (including no longer being written in TypeScript and then compiled to GJS with a combination of `tsc` and a hack-job of a shell script running regexes) and available on the https://extensions.gnome.org.
+
+NOTE: this project is very early in development, little more than an overgrown experiment, right now!
--- prefs.js
+++ prefs.js
@@ -10,82 +10,82 @@ function init() {
 }
 
 function fillPreferencesWindow(window) {
-  // Use the same GSettings schema as in `extension.js`
-  const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.autotilingwm');
+    // Use the same GSettings schema as in `extension.js`
+    const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.autotilingwm');
 
-  // Create a preferences page and group
-  const page = new Adw.PreferencesPage();
+    // Create a preferences page and group
+    const page = new Adw.PreferencesPage();
 
-  const group = new Adw.PreferencesGroup({
-    title: 'General settings'
-  });
-  page.add(group);
+    const group = new Adw.PreferencesGroup({
+        title: 'General settings'
+    });
+    page.add(group);
 
-  const group2 = new Adw.PreferencesGroup({
-    title: 'Appearance settings'
-  });
-  page.add(group2)
+    const group2 = new Adw.PreferencesGroup({
+        title: 'Appearance settings'
+    });
+    page.add(group2)
 
-  // Create a new preferences row
-  const row = new Adw.ActionRow({
-    title: 'Enable Auto Window Tiling',
-    subtitle: 'When enabled, this extension will manage tiling your windows on the screen.'
-  });
-  group.add(row);
+    // Create a new preferences row
+    const row = new Adw.ActionRow({
+        title: 'Enable Auto Window Tiling',
+        subtitle: 'When enabled, this extension will manage tiling your windows on the screen.'
+    });
+    group.add(row);
 
-  // Create the switch and bind its value
-  const toggle = new Gtk.Switch({
-    active: settings.get_boolean('tiling-on'),
-    valign: Gtk.Align.CENTER,
-  });
-  settings.bind(
-    'tiling-on',
-    toggle,
-    'active',
-    Gio.SettingsBindFlags.DEFAULT
-  );
+    // Create the switch and bind its value
+    const toggle = new Gtk.Switch({
+        active: settings.get_boolean('tiling-on'),
+        valign: Gtk.Align.CENTER,
+    });
+    settings.bind(
+        'tiling-on',
+        toggle,
+        'active',
+        Gio.SettingsBindFlags.DEFAULT
+    );
 
-  // Add the switch to the row
-  row.add_suffix(toggle);
-  row.activatable_widget = toggle;
+    // Add the switch to the row
+    row.add_suffix(toggle);
+    row.activatable_widget = toggle;
 
-  const row2 = new Adw.ActionRow({
-    title: 'Inner gaps',
-    subtitle: 'Amount of space in pixels left between windows'
-  });
-  group2.add(row2);
+    const row2 = new Adw.ActionRow({
+        title: 'Inner gaps',
+        subtitle: 'Amount of space in pixels left between windows'
+    });
+    group2.add(row2);
 
-  // Create the sliders for gaps
-  const innerGaps = Gtk.SpinButton.new_with_range(0, 30, 1);
-  innerGaps.set_margin_top(10);
-  innerGaps.set_margin_bottom(10);
-  settings.bind(
-    'inner-gaps',
-    innerGaps,
-    'value',
-    Gio.SettingsBindFlags.DEFAULT
-  );
-  row2.add_suffix(innerGaps);
-  row2.activatable_widget = innerGaps;
+    // Create the sliders for gaps
+    const innerGaps = Gtk.SpinButton.new_with_range(0, 30, 1);
+    innerGaps.set_margin_top(10);
+    innerGaps.set_margin_bottom(10);
+    settings.bind(
+        'inner-gaps',
+        innerGaps,
+        'value',
+        Gio.SettingsBindFlags.DEFAULT
+    );
+    row2.add_suffix(innerGaps);
+    row2.activatable_widget = innerGaps;
 
-  const row3 = new Adw.ActionRow({
-    title: 'Outer gaps',
-    subtitle: 'Amount of space in pixels left between all the windows and the borders of the screen.'
-  });
-  group2.add(row3);
+    const row3 = new Adw.ActionRow({
+        title: 'Outer gaps',
+        subtitle: 'Amount of space in pixels left between all the windows and the borders of the screen.'
+    });
+    group2.add(row3);
 
-  const outerGaps = Gtk.SpinButton.new_with_range(0, 30, 1);
-  outerGaps.set_margin_top(10);
-  outerGaps.set_margin_bottom(10);
-  settings.bind(
-    'outer-gaps',
-    outerGaps,
-    'value',
-    Gio.SettingsBindFlags.DEFAULT
-  );
-  row3.add_suffix(outerGaps);
-  row3.activatable_widget = outerGaps;
+    const outerGaps = Gtk.SpinButton.new_with_range(0, 30, 1);
+    outerGaps.set_margin_top(10);
+    outerGaps.set_margin_bottom(10);
+    settings.bind(
+        'outer-gaps',
+        outerGaps,
+        'value',
+        Gio.SettingsBindFlags.DEFAULT
+    );
+    row3.add_suffix(outerGaps);
+    row3.activatable_widget = outerGaps;
 
-  // Add our page to the window
-  window.add(page);
+    // Add our page to the window
+    window.add(page);
 }
